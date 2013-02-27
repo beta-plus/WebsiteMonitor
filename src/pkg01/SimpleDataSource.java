7284c50f885edb7bd4ca24ad4198e91d8a27da23
James Finney <jay.finney93@gmail.com>
2013-02-27 14:21:13 +0000

    test abc

--- src/pkg01/SimpleDataSource.java
+++ src/pkg01/SimpleDataSource.java
@@ -14,7 +14,7 @@ public class SimpleDataSource
     private static String password;
     
     /**
-     * @param fileName
+     * @param fileNam
      * @throws IOException
      * @throws ClassNotFoundException 
      */
