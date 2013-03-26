f4efda93ca01a2993378d31f2c71139ee61d1082
James Finney <jay.finney93@gmail.com>
2013-03-26 04:35:41 +0000

    RSS update

--- src/java/org/betaplus/testcases/RSSMonitor.java
+++ src/java/org/betaplus/testcases/RSSMonitor.java
@@ -12,138 +12,54 @@ package org.betaplus.testcases;
  * @version 1.0
  */
 
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.io.SyndFeedInput;
-import com.sun.syndication.io.XmlReader;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
-import java.util.Iterator;
+import java.util.Scanner;
 
 public class RSSMonitor
 {
-    final String url;
-    final URL feedSource;
-    final SyndFeedInput input;
-    final SyndFeed feed;
-    final Statement stat;
-    
-    public RSSMonitor(String inputUrl) throws Exception
+    public static void main(String[] args) throws Exception
     {
-        url = inputUrl;
-        feedSource = new URL(url);
-        input = new SyndFeedInput();
-        feed = input.build(new XmlReader(feedSource));
+        String urlFeed = "";
+        Boolean urlValid = false;
+        System.out.println("Starting RSS Monitor...");
         
-        SimpleDataSource.init("data/database.properties");
+        System.out.println("Please enter the feed URL...");
+        Scanner in = new Scanner(System.in);
         
-        // Get and make the connection
-        Connection conn = SimpleDataSource.getConnection();
-        
-        stat = conn.createStatement();
-    }
-    
-    public void displayFeed()
-    {
-        System.out.println("Title = " + feed.getTitle());
-        System.out.println("Description = " + feed.getDescription() + "\n");
-        
-        for (Iterator i = feed.getEntries().iterator(); i.hasNext();)
+        do
         {
-            SyndEntry entry = (SyndEntry) i.next();
-            System.out.println(entry.getTitle());
-            System.out.println(entry.getDescription().getValue());
-            System.out.println(entry.getPublishedDate());
-            System.out.println(entry.getLink() + "\n");         
-        }
-    }
-    
-    public void insertDatabase() throws SQLException
-    {
-        ResultSet checkUrl = stat.executeQuery("SELECT UrlId,Url FROM urls WHERE url= '" + url + "'");
-        
-        if (checkUrl.next())
-        {
-            int changesMade = 0;
-            int newInserts = 0;
-            System.out.println("Already inserted once! hahaha.");
-            
-            ResultSet getUrlId = stat.executeQuery("SELECT UrlId FROM urls WHERE url= '" + url + "'");
-            getUrlId.next();
-            String urlId = getUrlId.getString("UrlId");
-            System.out.println("UrlId = " + urlId);
-            
-            for (Iterator i = feed.getEntries().iterator(); i.hasNext();)
+            urlFeed = in.nextLine();
+            String checkExt = urlFeed.substring(urlFeed.length() - 3);
+            if ("xml".equals(checkExt))
             {
-                SyndEntry entry = (SyndEntry) i.next();    
-
-                String linkTitle = entry.getTitle().replace("'", "");
-                String linkDes = entry.getDescription().getValue().replace("'", "");
-                Date linkPubDate = entry.getPublishedDate();
-                String linkLink = entry.getLink().replace("'", "");
+                urlValid = true;
+                try
+                {
+                    URL test = new URL(urlFeed);
+                } catch (MalformedURLException ex)
+                {
+                    urlValid = false;
+                }
                 
-                ResultSet checkFeeds = stat.executeQuery("SELECT LinkPubDate FROM rss WHERE LinkTitle= '" + linkTitle + "'");
-                if (checkFeeds.next())
+                if (urlValid)
                 {
-                    String oldDate = checkFeeds.getString("LinkPubDate");
-                    String newDate = linkPubDate.toString();
-                    
-                    System.out.println("Link Title = " + linkTitle);
-                    System.out.println("Old Date = " + oldDate);
-                    System.out.println("New Date = " + newDate);
-                    
-                    if (!newDate.equals(oldDate))
-                    {
-                        stat.execute("UPDATE rss SET LinkDes='" + linkDes
-                                + "',LinkPubDate='" + linkPubDate + "',LinkLink='"
-                                + linkLink + "'WHERE LinkTitle='" + linkTitle + "')");
-                        changesMade += 1;
-                        System.out.println("New!\n");
-                    } else
-                    {
-                        System.out.println("Not new!");
-                    }
-                } else
-                {
-                    stat.execute("INSERT INTO rss (FeedTitle,FeedDes,LinkTitle,LinkDes,LinkPubDate,LinkLink,UrlId)"
-                        + "VALUES('" + feed.getTitle() + "','" + feed.getDescription() + "','"
-                        + linkTitle + "','" + linkDes + "','" + linkPubDate + "','"
-                        + linkLink+ "','" + urlId + "')");
-                    newInserts += 1;
+                    break;
                 }
             }
-            
-            System.out.println("Changes made = " + changesMade);
-            System.out.println("New inserts made = " + newInserts);
-        } else
-        {
-            System.out.println("First time!");
-            
-            stat.execute("INSERT INTO urls (Url, Type) VALUES('" + url + "','rss')");
-            
-            ResultSet getUrlId = stat.executeQuery("SELECT UrlId FROM urls WHERE url= '" + url + "'");
-            getUrlId.next();
-            String urlId = getUrlId.getString("UrlId");
-            System.out.println("UrlId = " + urlId);
-            
-            for (Iterator i = feed.getEntries().iterator(); i.hasNext();)
-            {
-                SyndEntry entry = (SyndEntry) i.next();    
-
-                String linkTitle = entry.getTitle().replace("'", "");
-                String linkDes = entry.getDescription().getValue().replace("'", "");
-                Date linkPubDate = entry.getPublishedDate();
-                String linkLink = entry.getLink().replace("'", "");
-
-                stat.execute("INSERT INTO rss (FeedTitle,FeedDes,LinkTitle,LinkDes,LinkPubDate,LinkLink,UrlId)"
-                        + "VALUES('" + feed.getTitle() + "','" + feed.getDescription() + "','"
-                        + linkTitle + "','" + linkDes + "','" + linkPubDate + "','"
-                        + linkLink+ "','" + urlId + "')");
-            }
-        }
+            System.out.println("I'm sorry but the URL you provided isn't a suitable RSS feed. Please try another URL...");
+        } while (!urlValid);
+        
+        in.close();
+        
+        RSSMonitorFunctions monitor = new RSSMonitorFunctions(urlFeed);
+        
+        //System.out.println("Getting feed...");
+        //monitor.displayFeed();
+        
+        System.out.println("Inserting feed...");
+        monitor.insertDatabase();
+        
+        System.out.println("Insert complete!");
     }
 }
---
+++ src/java/org/betaplus/testcases/RSSMonitorFunctions.java
@@ -0,0 +1,149 @@
+package org.betaplus.testcases;
+
+/*
+ * Author: James Finney
+ * Title: RSS Monitor Functions
+ * Created: 01/02/2012
+ * Version: 1.0
+ */
+
+/**
+ * @author James Finney
+ * @version 1.0
+ */
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.Iterator;
+
+public class RSSMonitorFunctions
+{
+    final String url;
+    final URL feedSource;
+    final SyndFeedInput input;
+    final SyndFeed feed;
+    final Statement stat;
+    
+    public RSSMonitorFunctions(String inputUrl) throws Exception
+    {
+        url = inputUrl;
+        feedSource = new URL(url);
+        input = new SyndFeedInput();
+        feed = input.build(new XmlReader(feedSource));
+        
+        SimpleDataSource.init("data/database.properties");
+        
+        // Get and make the connection
+        Connection conn = SimpleDataSource.getConnection();
+        
+        stat = conn.createStatement();
+    }
+    
+    public void displayFeed()
+    {
+        System.out.println("Title = " + feed.getTitle());
+        System.out.println("Description = " + feed.getDescription() + "\n");
+        
+        for (Iterator i = feed.getEntries().iterator(); i.hasNext();)
+        {
+            SyndEntry entry = (SyndEntry) i.next();
+            System.out.println(entry.getTitle());
+            System.out.println(entry.getDescription().getValue());
+            System.out.println(entry.getPublishedDate());
+            System.out.println(entry.getLink() + "\n");         
+        }
+    }
+    
+    public void insertDatabase() throws SQLException
+    {
+        ResultSet checkUrl = stat.executeQuery("SELECT UrlId,Url FROM urls WHERE url= '" + url + "'");
+        
+        if (checkUrl.next())
+        {
+            int changesMade = 0;
+            int newInserts = 0;
+            System.out.println("Already inserted once! hahaha.");
+            
+            ResultSet getUrlId = stat.executeQuery("SELECT UrlId FROM urls WHERE url= '" + url + "'");
+            getUrlId.next();
+            String urlId = getUrlId.getString("UrlId");
+            System.out.println("UrlId = " + urlId);
+            
+            for (Iterator i = feed.getEntries().iterator(); i.hasNext();)
+            {
+                SyndEntry entry = (SyndEntry) i.next();    
+
+                String linkTitle = entry.getTitle().replace("'", "");
+                String linkDes = entry.getDescription().getValue().replace("'", "");
+                Date linkPubDate = entry.getPublishedDate();
+                String linkLink = entry.getLink().replace("'", "");
+                
+                ResultSet checkFeeds = stat.executeQuery("SELECT LinkPubDate FROM rss WHERE LinkTitle= '" + linkTitle + "'");
+                if (checkFeeds.next())
+                {
+                    String oldDate = checkFeeds.getString("LinkPubDate");
+                    String newDate = linkPubDate.toString();
+                    
+                    System.out.println("Link Title = " + linkTitle);
+                    System.out.println("Old Date = " + oldDate);
+                    System.out.println("New Date = " + newDate);
+                    
+                    if (!newDate.equals(oldDate))
+                    {
+                        stat.execute("UPDATE rss SET LinkDes='" + linkDes
+                                + "',LinkPubDate='" + linkPubDate + "',LinkLink='"
+                                + linkLink + "'WHERE LinkTitle='" + linkTitle + "')");
+                        changesMade += 1;
+                        System.out.println("New!\n");
+                    } else
+                    {
+                        System.out.println("Not new!");
+                    }
+                } else
+                {
+                    stat.execute("INSERT INTO rss (FeedTitle,FeedDes,LinkTitle,LinkDes,LinkPubDate,LinkLink,UrlId)"
+                        + "VALUES('" + feed.getTitle() + "','" + feed.getDescription() + "','"
+                        + linkTitle + "','" + linkDes + "','" + linkPubDate + "','"
+                        + linkLink+ "','" + urlId + "')");
+                    newInserts += 1;
+                }
+            }
+            
+            System.out.println("Changes made = " + changesMade);
+            System.out.println("New inserts made = " + newInserts);
+        } else
+        {
+            System.out.println("First time!");
+            
+            stat.execute("INSERT INTO urls (Url, Type) VALUES('" + url + "','rss')");
+            
+            ResultSet getUrlId = stat.executeQuery("SELECT UrlId FROM urls WHERE url= '" + url + "'");
+            getUrlId.next();
+            String urlId = getUrlId.getString("UrlId");
+            System.out.println("UrlId = " + urlId);
+            
+            for (Iterator i = feed.getEntries().iterator(); i.hasNext();)
+            {
+                SyndEntry entry = (SyndEntry) i.next();    
+
+                String linkTitle = entry.getTitle().replace("'", "");
+                String linkDes = entry.getDescription().getValue().replace("'", "");
+                Date linkPubDate = entry.getPublishedDate();
+                String linkLink = entry.getLink().replace("'", "");
+
+                stat.execute("INSERT INTO rss (FeedTitle,FeedDes,LinkTitle,LinkDes,LinkPubDate,LinkLink,UrlId)"
+                        + "VALUES('" + feed.getTitle() + "','" + feed.getDescription() + "','"
+                        + linkTitle + "','" + linkDes + "','" + linkPubDate + "','"
+                        + linkLink+ "','" + urlId + "')");
+            }
+        }
+    }
+}
