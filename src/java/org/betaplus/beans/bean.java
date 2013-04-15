5f1d2e40701942d5bafab37af183ae1c69a7f31c
James Finney <jay.finney93@gmail.com>
2013-04-15 04:28:37 +0100

    UI Update

--- src/java/org/betaplus/beans/bean.java
+++ src/java/org/betaplus/beans/bean.java
@@ -1,13 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.betaplus.beans;
 
 /**
  *
  * @author Jay
  */
-public class bean {
-    
+public class bean
+{    
 }
